7e54f7e13fe782422ce27f3d5bd93a3e8dce7121
Muhammad5569 <777muhammad5569@gmail.com>
2025-08-15 08:38:35 +0500

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const testRoutes = require('./routes/test')
 const userRoutes = require('./routes/users')    
 const listeningRoutes = require('./routes/sections/listening')
@@ -13,7 +14,7 @@ mongoose.connect('mongodb://localhost:27017/techquest')
 .catch(err => console.error('MongoDB connection error:', err));
 
 app.use(express.json())
-
+app.use(cors())
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
